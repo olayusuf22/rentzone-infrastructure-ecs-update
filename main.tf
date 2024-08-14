3da1b4fabf750ee97afb607fd77e06f5f2551128
OYusuf <olayusuf822@gmail.com>
2024-08-14 15:58:18 -0500

    update

---
+++ main.tf
@@ -0,0 +1,44 @@
+locals {
+  region       = var.region
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+# create vpc module
+module "vpc" {
+  source                       = "git@github.com:olayusuf22/terraform-modules-update.git//vpc"
+  region                       = local.region
+  project_name                 = local.project_name
+  environment                  = local.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# create nat-gateway
+module "nat-gateway" {
+  source                     = "git@github.com:olayusuf22/terraform-modules-update.git//nat-gateway"
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+}
+
+# create security group
+module "nat-gateway" {
+  source       = "git@github.com:olayusuf22/terraform-modules-update.git//security-groups"
+  project_name = local.project_name
+  environment  = local.environment
+  vpc_id       = module.vpc.vpc_cidr
+  ssh_ip       = var.ssh_ip
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+# environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+# vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+
+#security group variables
+variable "ssh_ip" {}
